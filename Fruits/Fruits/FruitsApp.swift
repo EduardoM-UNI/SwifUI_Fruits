2e8b80c2e715b07d76d8142982fdf8c9728eef42
Eduardo Monroy <edu.wap2@gmail.com>
2021-07-24 22:21:21 +0200

    First Commit

---
+++ Fruits/Fruits/FruitsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FruitsApp.swift
+//  Fruits
+//
+//  Created by Eduardo Monroy Husillos on 24/7/21.
+//
+
+import SwiftUI
+
+@main
+struct FruitsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
