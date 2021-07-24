9964a73542755274d2b36bf97157979abf7efbd4
Eduardo Monroy <edu.wap2@gmail.com>
2021-07-25 01:46:11 +0200

    Add Fruit cardView

---
+++ Fruits/Fruits/View/StarButtonView.swift
@@ -0,0 +1,37 @@
+//
+//  StarButtonView.swift
+//  Fruits
+//
+//  Created by Eduardo Monroy Husillos on 25/7/21.
+//
+
+import SwiftUI
+
+struct StarButtonView: View {
+    
+    // MARK: - PROPERTIES
+    
+    // MARK: - BODY
+    var body: some View {
+        Button(action: {print ("Exit the onboarding")}) {
+            HStack(spacing: 8) {
+                Text("Start")
+                
+                Image(systemName: "arrow.right.circle")
+                    .imageScale(.large)
+            }
+            .padding(.horizontal, 16)
+            .padding(.vertical, 10)
+            .background(Capsule().strokeBorder(Color.white, lineWidth: 1.25))
+        }     //: BUTTOM
+        .accentColor(.white)
+}
+}
+    // MARK: - PREVIEW
+struct StarButtonView_Previews: PreviewProvider {
+    static var previews: some View {
+        StarButtonView()
+            .preferredColorScheme(.dark)
+            .previewLayout(.sizeThatFits)
+    }
+}
